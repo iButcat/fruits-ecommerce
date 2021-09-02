2828eb38310d76478480d341797b7e1c2af5c4c4
iButcat <Butcat@protonmail.com>
2021-09-02 14:44:43 +0200

    main init db

--- api/main.go
+++ api/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	// internal pkg
+	"ecommerce/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("this is a test to see if docker is working")
+	config, err := config.LoadConfig("./config")
+	if err != nil {
+		log.Println(err)
+	}
+
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("error while initializing db...", err)
+	}
+
+	if db != nil {
+		log.Println("db connected....")
+	}
 }
