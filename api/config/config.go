74839adde283357088a1854984018d4787205521
iButcat <Butcat@protonmail.com>
2021-09-02 14:43:47 +0200

    configuration env var

---
+++ api/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import "github.com/spf13/viper"
+
+// Containing different fields as DSN, port, etc...
+type Config struct {
+	Dsn  string `mapstructure:"dsn"`
+	Port string `mapstructure:"port"`
+}
+
+// Load our config in our struct to use it.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.AddConfigPath(".env")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config, err
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
