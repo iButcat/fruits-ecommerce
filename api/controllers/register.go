d6294d54754cf297d7f1bcededa1915cfd564797
iButcat <Butcat@protonmail.com>
2021-09-03 16:50:34 +0200

    add some checker/validator for our registration

--- api/controllers/register.go
+++ api/controllers/register.go
@@ -8,19 +8,44 @@ import (
 
 	// internal pkg
 	"ecommerce/models"
+	"ecommerce/utils.go"
 
 	"github.com/gin-gonic/gin"
 )
 
-type User models.User
-
-func (u User) RegisterUser(ctx *gin.Context) {
+func (c controller) Register(ctx *gin.Context) {
+	var user = new(models.User)
 	data, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
-		log.Println("err while trying to register user: ", err)
+		log.Println("err while trying to read request: ", err)
+		return
 	}
-	if err := json.Unmarshal(data, &u); err != nil {
+
+	if err := json.Unmarshal(data, &user); err != nil {
 		log.Println("err while trying to unmarshal data: ", err)
+		return
 	}
-	fmt.Println(u)
+
+	var lengthPassword = len(user.Password)
+	var lengthUsername = len(user.Username)
+	if !utils.ValidateIfNotEmpty(lengthPassword, lengthUsername) {
+		ctx.JSON(400, gin.H{"error": "missing fields password or username"})
+		ctx.Abort()
+		return
+	}
+
+	if !utils.RegexEmailChecker(user.Email) {
+		ctx.JSON(400, gin.H{"error": "email is not valid"})
+		ctx.Abort()
+		return
+	}
+
+	ok, err := c.repository.Create(ctx, user)
+	if err != nil {
+		log.Println("error while saving user: ", err)
+		return
+	}
+	fmt.Println(ok)
+
+	ctx.JSON(201, gin.H{"message": "User has been created"})
 }
