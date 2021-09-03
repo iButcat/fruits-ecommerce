795f00579f10b57750a0667864b8eda38fd8ad19
iButcat <Butcat@protonmail.com>
2021-09-03 16:53:54 +0200

    email checker and validator fields

--- api/utils.go/utils.go
+++ api/utils.go/utils.go
@@ -2,12 +2,14 @@ package utils
 
 import "regexp"
 
+// basic regex that check if corrects email
 func RegexEmailChecker(email string) bool {
 	var validator = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
 	var regexEmail = regexp.MustCompile(validator)
 	return regexEmail.MatchString(email)
 }
 
+// check that our different fields are not empty
 func ValidateIfNotEmpty(fields ...int) bool {
 	for _, field := range fields {
 		if field == 0 {
