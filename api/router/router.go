c9392bcd62f3b5991c4ca076b10f2941bcaa26e2
iButcat <Butcat@protonmail.com>
2021-09-03 16:51:00 +0200

    router

---
+++ api/router/router.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"ecommerce/controllers"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter(controller controllers.Controllers, logger log.Logger) *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	v1 := router.Group("v1")
+	{
+		userGroup := v1.Group("user")
+		{
+			userGroup.POST("/register", controller.Register)
+		}
+	}
+
+	return router
+}
