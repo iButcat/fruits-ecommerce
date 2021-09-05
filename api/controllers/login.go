aed7b6b9fd95bd94a40053a780e5badd4a5156a3
iButcat <Butcat@protonmail.com>
2021-09-05 17:39:39 +0200

    add deferencing and basic auth checker

--- api/controllers/login.go
+++ api/controllers/login.go
@@ -2,19 +2,26 @@ package controllers
 
 import (
 	"ecommerce/models"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (c controller) Login(ctx *gin.Context) {
-	//username, password, ok := ctx.Request.BasicAuth()
+	username, password, ok := ctx.Request.BasicAuth()
+
+	log.Println("username: ", username)
+	log.Println("password: ", password)
+
 	var userModel = models.User{}
-	data, err := c.repository.Get(ctx, userModel, "1")
+
+	data, err := c.repository.Get(ctx, &userModel, username, password)
 	if err != nil {
 		ctx.JSON(400, gin.H{"error": err})
 	}
 	data = userModel
-	//fmt.Println("is auth: ", ok)
+	fmt.Println("is auth: ", ok)
 
 	ctx.JSON(200, gin.H{"data": data})
 }
