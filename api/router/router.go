9429549532fc1f72b31d777702e2af193adcbbd4
iButcat <Butcat@protonmail.com>
2021-09-07 17:12:58 +0200

    add new controller router

--- api/router/router.go
+++ api/router/router.go
@@ -19,6 +19,14 @@ func NewRouter(controller controllers.Controllers, logger log.Logger) *gin.Engin
 			userGroup.POST("/register", controller.Register)
 			userGroup.POST("/login", controller.Login)
 		}
+
+		productsGroup := v1.Group("products")
+		{
+			productsGroup.GET("/get", controller.GetProducts)
+			productsGroup.PUT("/update", controller.UpdateProducts)
+			productsGroup.POST("/create", controller.CreateProducts)
+			productsGroup.DELETE("/delete", controller.DeleteProducts)
+		}
 	}
 
 	return router
