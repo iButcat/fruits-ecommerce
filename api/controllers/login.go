919b91d221906c2b2f374bbcdec5d5f42b517c3f
iButcat <Butcat@protonmail.com>
2021-09-08 23:08:17 +0200

    add user checker from db

--- api/controllers/login.go
+++ api/controllers/login.go
@@ -2,26 +2,50 @@ package controllers
 
 import (
 	"ecommerce/models"
-	"fmt"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
-func (c controller) Login(ctx *gin.Context) {
-	username, password, ok := ctx.Request.BasicAuth()
+var (
+	errWrongLoginCredentials = errors.New("err wrong credentials has been submitted")
+)
 
-	log.Println("username: ", username)
-	log.Println("password: ", password)
+func (c controller) Login(ctx *gin.Context) {
+	//username, password, ok := ctx.Request.BasicAuth()
 
 	var userModel = models.User{}
 
-	data, err := c.repository.Get(ctx, &userModel, username, password)
+	// TODO: move it into a function
+	var args = make(map[string]interface{})
+
+	readRequest, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
-		ctx.JSON(400, gin.H{"error": err})
+		log.Println(err)
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return
 	}
-	data = userModel
-	fmt.Println("is auth: ", ok)
+
+	json.Unmarshal(readRequest, &args)
+
+	log.Println(args)
+	data, err := c.repository.Get(ctx, &models.User{}, args)
+	if err != nil {
+		log.Println(err)
+		ctx.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	userRepo := data.(*models.User)
+
+	if userRepo.Username != userModel.Username || userRepo.Password != userModel.Password {
+		log.Println(errWrongLoginCredentials)
+		ctx.JSON(400, gin.H{"error": errWrongLoginCredentials})
+		return 
+	}
 
 	ctx.JSON(200, gin.H{"data": data})
 }
