64a192ee36971c8f2e5da0c2a7397e2096af154b
iButcat <Butcat@protonmail.com>
2021-09-11 15:26:10 +0200

    jwt middleware working

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -1,10 +1,99 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"ecommerce/models"
+	"errors"
+	"log"
+	"time"
 
-// this is our custom jwt token reader
-func CustomJwtMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+)
 
+type LoginVals struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var (
+	identityKey = "id"
+
+	errNoUsernamePassowrd = errors.New("username or password has not be submited")
+)
+
+func CustomJwtMiddleware() *jwt.GinJWTMiddleware {
+	var loginVals = LoginVals{}
+
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "test zone",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*models.User); ok {
+				return jwt.MapClaims{
+					identityKey: v.Username,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &models.User{
+				Username: claims[identityKey].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+
+			if err := c.ShouldBind(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			userID := loginVals.Username
+			password := loginVals.Password
+
+			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+				return &models.User{
+					Username: userID,
+				}, nil
+			}
+
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*models.User); ok && v.Username == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		// TokenLookup is a string in the form of "<source>:<name>" that is used
+		// to extract token from the request.
+		// Optional. Default value "header:Authorization".
+		// Possible values:
+		// - "header:<name>"
+		// - "query:<name>"
+		// - "cookie:<name>"
+		// - "param:<name>"
+		TokenLookup: "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+
+		// TokenHeadName is a string in the header. Default value is "Bearer"
+		TokenHeadName: "Bearer",
+
+		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
+		TimeFunc: time.Now,
+	})
+
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
 	}
+	return authMiddleware
 }
--- api/router/router.go
+++ api/router/router.go
@@ -6,7 +6,9 @@ import (
 	// internal pkg
 	"ecommerce/controller"
 	"ecommerce/middleware"
+	"ecommerce/models"
 
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,8 +29,11 @@ func (cr controllersRouter) NewRouter(logger log.Logger) *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
-	// call gin jwt
-	router.Use(middleware.CustomJwtMiddleware())
+
+	err := middleware.CustomJwtMiddleware().MiddlewareInit()
+	if err != nil {
+		log.Fatal("authMiddleware", err.Error())
+	}
 
 	v1 := router.Group("v1")
 	{
@@ -36,13 +41,26 @@ func (cr controllersRouter) NewRouter(logger log.Logger) *gin.Engine {
 		{
 			userGroup.POST("/register", cr.authController.Register)
 			userGroup.POST("/login", cr.authController.Login)
+			userGroup.POST("/token", middleware.CustomJwtMiddleware().LoginHandler) // generate jwt token
+		}
+
+		authTestGroup := v1.Group("authtest")
+		authTestGroup.Use(middleware.CustomJwtMiddleware().MiddlewareFunc())
+		{
+			authTestGroup.GET("/hello", func(ctx *gin.Context) {
+				claims := jwt.ExtractClaims(ctx)
+				user, _ := ctx.Get("id")
+				ctx.JSON(200, gin.H{"userId": claims["id"],
+					"username": user.(*models.User).Username,
+					"text":     "Hello World"})
+			})
 		}
 
 		productsGroup := v1.Group("products")
 		{
 			productsGroup.GET("/get", cr.productsController.GetById)
-			productsGroup.PUT("/getall", cr.productsController.GetAll)
-			productsGroup.POST("/create", cr.productsController.Update)
+			productsGroup.GET("/getall", cr.productsController.GetAll)
+			productsGroup.PATCH("/update", cr.productsController.Update)
 			productsGroup.DELETE("/delete", cr.productsController.Delete)
 		}
 	}
