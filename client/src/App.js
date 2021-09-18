215c39d48bfc25c26561a011014ca158e78e61d1
iButcat <Butcat@protonmail.com>
2021-09-18 18:21:41 +0200

    add router and pages/componenets     '''

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
+import Layout from './hocs/Layout';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+        <Fragment>
+          <Layout>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/products' component={Products} />
+            <Route exact path='/cart' component={Cart} />
+          </Layout>
+        </Fragment>
+        </Switch>
+      </Router>
     </div>
   );
 }
