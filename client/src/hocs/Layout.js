7bb4388d77fbb3f55782aaed99fe1eca0f6756d9
iButcat <Butcat@protonmail.com>
2021-09-24 11:52:08 +0200

    etc

--- client/src/hocs/Layout.js
+++ client/src/hocs/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import Footer from '../components/Footer';
 import NavbarI from '../components/Navbar';
 
 function Layout(props) {
     return (
         <div>
-            <NavbarI isLogged={Boolean(localStorage.getItem('logged'))} />
+            <NavbarI/>
                 {props.children}
             <Footer/>
         </div>
