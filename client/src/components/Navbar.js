37058e28f8d6dac371cc83e22b034ccf9c5f0757
iButcat <Butcat@protonmail.com>
2021-09-24 11:52:27 +0200

    add link cart + products

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import  { Navbar, Nav, Container } from 'react-bootstrap';
 
 import LogoutButton from '../components/LogoutButton';
+import getValueLocalStorage from '../utils/Utils';
 
-function NavbarI(props) {
-    const [logged, setLogged] = useState(false); 
+function NavbarI() {
     
-    console.log("PROPS LOGGED", props.isLogged);
-
-    window.addEventListener("storage", (e) => {
-        setLogged(Boolean(localStorage.getItem('logged')));
-    });
-
     return (
         <Navbar bg="light" expand="lg">
             <Container> 
@@ -19,13 +13,15 @@ function NavbarI(props) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    {props.isLogged === true ? 
+                    {getValueLocalStorage('logged') === "true" ? 
                     <Nav.Link><LogoutButton/></Nav.Link> :
-                    <div> 
+                    <>
                     <Nav.Link href="login">Login</Nav.Link>
                     <Nav.Link href="register">Register</Nav.Link>
-                    </div>
+                    </>
                     }
+                    <Nav.Link href="cart">Cart</Nav.Link>
+                    <Nav.Link href="products">Products</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
