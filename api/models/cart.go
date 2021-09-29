7710281fc2d9315fdcc9c92637d59460358e8e37
iButcat <Butcat@protonmail.com>
2021-09-29 20:39:45 +0200

    foreign key

--- api/models/cart.go
+++ api/models/cart.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Cart struct {
 	gorm.Model
-	CartItems  []CartItem `gorm:"foreignkey:ID;association_foreignkey:ID" json:"products"`
+	CartItems  []CartItem `gorm:"foreignkey:CartID;association_foreignkey:ID" json:"products"`
 	Quantity   int        `json:"cart_items"`
 	TotalPrice float64    `json:"total_price"`
 	Username   string     `json:"username"`
