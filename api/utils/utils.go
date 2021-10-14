8065a98eec6780eb84a944867f41dd67cd63eb78
iButcat <Butcat@protonmail.com>
2021-10-14 13:04:30 +0200

    add calcul total and discount price

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"regexp"
+
+	"ecommerce/models"
 )
 
 // basic regex that check if corrects email
@@ -32,14 +34,41 @@ func ToString(field interface{}) string {
 	return string(converted)
 }
 
-func CalculateDiscountBanana(quantity int, totalPrice float64) float64 {
+func CalculateDiscountApples(name string, quantity int, price float64) float64 {
 	var result float64
-	var biggerThanSeven bool
-	if quantity >= 7 {
-		biggerThanSeven = true
-	}
-	if biggerThanSeven {
-		result = totalPrice - (totalPrice * .10)
+	if name == "apples" {
+		var biggerThanSeven bool
+		if quantity >= 7 {
+			biggerThanSeven = true
+		}
+		if biggerThanSeven {
+			result = (float64(quantity) * price / 100)
+		}
+	} else {
+		return 0
 	}
 	return result
+}
+
+func CalculateDiscountSet(setFruits map[string]int, price float64) float64 {
+	var isSet = make([]bool, 2)
+	for name, quantity := range setFruits {
+		if name == "pears" && quantity >= 4 {
+			isSet = append(isSet, true)
+		} else if name == "bananas" && quantity >= 2 {
+			isSet = append(isSet, true)
+		}
+	}
+	if len(isSet) == 2 && isSet[0] && isSet[1] {
+		price = price / 300
+	}
+	return price
+}
+
+func CalculateTotalPriceCart(cart models.Cart) float64 {
+	var totalPrice float64
+	for _, cartItem := range cart.CartItems {
+		totalPrice += cartItem.TotalPrice
+	}
+	return totalPrice
 }
